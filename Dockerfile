f9377ca3c7608a55bd4156aed5e8a491018d8721
amitvermaio <amitvns159@gmail.com>
2026-04-28 15:23:50 +0530

    change

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@
 FROM maven:3.9.6-eclipse-temurin-17 AS build
 WORKDIR /app
 
+
 COPY pom.xml ./
 COPY mvnw ./
 COPY mvnw.cmd ./
--- src/main/java/com/example/studentmarketplace/util/JwtUtil.java
+++ src/main/java/com/example/studentmarketplace/util/JwtUtil.java
@@ -1,16 +1,20 @@
 package com.example.studentmarketplace.util;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+// changes
 
 @Component
 public class JwtUtil {
