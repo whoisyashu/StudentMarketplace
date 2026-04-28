d5c939a974f84dd2e9ad6a2c630c5e71035793f6
whoisyashu <maheshwariy077@gmail.com>
2026-04-28 13:47:14 +0530

    Created a Basic Structure of the Project

---
+++ src/main/java/com/example/studentmarketplace/StudentMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMarketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmarketplace/StudentMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
